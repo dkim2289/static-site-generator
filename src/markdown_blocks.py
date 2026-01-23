f1cbb0ef9ba99b2499ab7c51fee2d4290ae776a8
dkim2289 <dhkim.89.1497@icloud.com>
2026-01-23 22:15:05 +1100

    Block Types

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,13 @@
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    ULIST = "unordered_list"
+    OLIST = "ordered_list"
+
 def markdown_to_blocks(text):
     blocks = []
     separated_texts = text.split("\n\n")
@@ -6,3 +16,36 @@ def markdown_to_blocks(text):
             continue
         blocks.append(separated_text.strip())
     return blocks
+
+
+def block_to_block_type(text_block):
+    text_lines = text_block.split("\n")
+
+    if text_block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        return BlockType.HEADING
+
+    elif len(text_lines)>1 and text_lines[0].startswith("```") and text_lines[-1].endswith("```"):
+        return BlockType.CODE
+
+    elif text_block.startswith("> "):
+        for text_line in text_lines:
+            if not text_line.startswith("> "):
+                return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+
+    elif text_block.startswith("- "):
+        for text_line in text_lines:
+            if not text_line.startswith("- "):
+                return BlockType.PARAGRAPH
+        return BlockType.ULIST
+
+    elif text_block.startswith("1. "):
+        i = 1
+        for text_line in text_lines:
+            if not text_line.startswith(f"{i}. "):
+                return BlockType.PARAGRAPH
+            i+=1
+        return BlockType.OLIST
+
+    else:
+        return BlockType.PARAGRAPH
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,5 +1,5 @@
 import unittest
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import BlockType, markdown_to_blocks, block_to_block_type
 
 
 class TestMarkdownToHTML(unittest.TestCase):
@@ -45,6 +45,32 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+class TestBlockToBlockType(unittest.TestCase):
+    def test_paragraph(self):
+        text = "This is **bolded** paragraph"
+        self.assertEqual(block_to_block_type(text), BlockType.PARAGRAPH)
+
+    def test_heading(self):
+        text = "# Heading"
+        self.assertEqual(block_to_block_type(text), BlockType.HEADING)
+
+    def test_code(self):
+        text = "```\npython\nprint('Hello, World!')```"
+        self.assertEqual(block_to_block_type(text), BlockType.CODE)
+
+    def test_quote(self):
+        text = "> This is a quote"
+        self.assertEqual(block_to_block_type(text), BlockType.QUOTE)
+
+    def test_unordered_list(self):
+        text = "- This is an unordered list"
+        self.assertEqual(block_to_block_type(text), BlockType.ULIST)
+
+    def test_ordered_list(self):
+        text = "1. This is an ordered list\n2. This is another item\n3. This is a third item"
+        self.assertEqual(block_to_block_type(text), BlockType.OLIST)
+
 
 
 if __name__ == "__main__":
